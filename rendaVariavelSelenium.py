e67b47f177737c78540a6c8967363dc76a87a190
Nicolas Passos <nicolasduart21@gmail.com>
2023-07-28 18:50:22 -0400

    Ajustando apenas o texto de retorno

--- rendaVariavelSelenium.py
+++ rendaVariavelSelenium.py
@@ -28,7 +28,7 @@ for ativo in ativos:
         valorAtual = navegador.find_element('xpath','//*[@id="main-2"]/div[2]/div/div[1]/div/div[1]/div/div[1]/strong').text
         variacaoValorAtivo = navegador.find_element('xpath','//*[@id="main-2"]/div[2]/div/div[1]/div/div[1]/div/div[2]/span/b').text
 
-        resultadoAtivos.append(f'A cotação do ativo {ativo["ticker"]} está com o valor de {moeda}{valorAtual}, com variação do valor de {variacaoValorAtivo}.')
+        resultadoAtivos.append(f'A cotação do ativo {ativo["ticker"]} está com o valor de {moeda}{valorAtual} com variação do valor de {variacaoValorAtivo}.')
 
     if ativo['tipo'] == 'fundos-imobiliarios':
         moeda = navegador.find_element('xpath','//*[@id="main-2"]/div[2]/div[1]/div[1]/div/div[1]/span').text
@@ -37,7 +37,7 @@ for ativo in ativos:
         rendimento = navegador.find_element('xpath','//*[@id="dy-info"]/div/div[1]/strong').text
         dataPagRendimento = navegador.find_element('xpath','//*[@id="dy-info"]/div/div[2]/div[2]/div[2]/div/b').text
 
-        resultadoAtivos.append(f'A cotação do ativo {ativo["ticker"]} está com o valor de {moeda}{valorAtual}, com variação do valor de {variacaoValorAtivo}. O último rendimento deste ativo foi pago no dia {dataPagRendimento} no valor de {rendimento}.')
+        resultadoAtivos.append(f'A cotação do ativo {ativo["ticker"]} está com o valor de {moeda}{valorAtual} com variação do valor de {variacaoValorAtivo}. O último rendimento deste ativo foi pago no dia {dataPagRendimento} no valor de {rendimento}.')
 
 for resultado in resultadoAtivos:
     print(resultado)
